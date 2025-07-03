1398f065b6f82b8e5292632953523ee5b5d93a45
Manish Kumar <jangramanish2602@gmail.com>
2025-07-03 23:25:52 +0530

    import export

---
+++ import/script.js
@@ -0,0 +1,9 @@
+import {PI,sum} from "./math.js";
+
+console.log(sum(2,3));
+
+
+//import is new than require , it can import individual components
+//npm init used to install packagage.json and we have to set type:module in package.json
+//to use the import and export
+//different packages from the web can also be used using import
