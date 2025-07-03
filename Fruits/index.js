e171b75ef7450bb3142517015d5429784f032567
Manish Kumar <jangramanish2602@gmail.com>
2025-07-03 22:11:27 +0530

    import from directory

---
+++ Fruits/index.js
@@ -0,0 +1,7 @@
+const apple  = require("./apple");
+const bannana = require("./bannana");
+const mango = require("./mango");
+
+let fruitsarr = [apple,bannana,mango];
+
+module.exports=fruitsarr;
--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const var1 = require("./math");
 console.log(var1);
 console.log(var1.PI);
+
+const var2 = require("./Fruits");
+console.log(var2);
+console.log(var2[0],var2[0].name);
