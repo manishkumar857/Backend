d0ecedde64c775d45664b428fe98f18b9e7915f7
Manish Kumar <jangramanish2602@gmail.com>
2025-07-06 15:58:35 +0530

    routing and nodemon setup

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-console.dir(app); //app is a object
+// console.dir(app); //app is a object
 
 let port =3000;
 
@@ -9,9 +9,29 @@ app.listen(port, ()=>{
     console.log('app is listening on port')
 }) //ctrl + c is used to stop listening
 
-app.use((req,res)=>{
-    // console.dir(req) //req in the form of object so that js understand it (converted from simple text)
-    console.log("request recieved");
 
-    res.send("this is a basic response");
+app.get("/",(req,res)=>{
+    res.send("you contacted home page");
 })
+app.get("/help",(req,res)=>{
+    res.send("you contacted help page");
+})
+app.get("/about",(req,res)=>{
+    res.send("you contacted about page");
+})
+
+// app.get("*",(req,res)=>{
+//     res.send("this page doesn't exist");
+// })
+
+app.post("/",(req,res)=>{
+    res.send("post request recieved");
+})
+
+
+// app.use((req,res)=>{
+//     // console.dir(req) //req in the form of object so that js understand it (converted from simple text)
+//     console.log("request recieved");
+
+//     res.send("this is a basic response");
+// })
