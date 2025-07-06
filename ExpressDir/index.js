c95e18777980fcfca9e32e587891a9060cc2f76c
Manish Kumar <jangramanish2602@gmail.com>
2025-07-06 15:43:12 +0530

    basic response

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -8,3 +8,10 @@ let port =3000;
 app.listen(port, ()=>{
     console.log('app is listening on port')
 }) //ctrl + c is used to stop listening
+
+app.use((req,res)=>{
+    // console.dir(req) //req in the form of object so that js understand it (converted from simple text)
+    console.log("request recieved");
+
+    res.send("this is a basic response");
+})
