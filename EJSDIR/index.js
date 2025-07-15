5279d9a1020049d002a32bd632b0d85febb0b32f
Manish Kumar <jangramanish2602@gmail.com>
2025-07-15 11:31:42 +0530

    template

--- EJSDIR/index.js
+++ EJSDIR/index.js
@@ -1,7 +1,14 @@
 const express = require("express")
 const app = express()
-
+const path = require("path");
 const port =8080;
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"))
+
+app.get("/",(req,res)=>{
+    res.render("home.ejs");
+})
 
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
