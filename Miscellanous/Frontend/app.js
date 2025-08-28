15d2a3932220fcc8ef4c4be7856269b905661390
Manish Kumar <jangramanish2602@gmail.com>
2025-08-28 21:57:48 +0530

    constructor

--- Miscellanous/Frontend/app.js
+++ Miscellanous/Frontend/app.js
@@ -14,3 +14,17 @@ function PersonMaker(name,age){
     let p2 = PersonMaker("Manisha",21);//creates its own copy of talk function which takes lots of memory
 
 }
+
+
+//constructor- doesn't return anything and start with capital letter
+function Person(name,age){
+    this.name=name;
+    this.age=age;
+    console.log(this);
+}
+Person.prototype.talk= function(){
+    console.log(`this is the name ${this.name} `);
+}
+
+let p1=new Person("Manish",23);
+let p2=new Person("Manisha",21);
