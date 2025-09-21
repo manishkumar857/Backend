91cba803fedfbe830b695431bffef9769de6e289
Manish Kumar <jangramanish2602@gmail.com>
2025-09-21 18:49:56 +0530

    Rest Api /posts

---
+++ REST_CLASS/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+const path = require("path");
+
+app.use(express.urlencoded({extended:true}));
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public")));
+
+let posts = [
+    { username : "Manish",
+      content  : "Hardwork is the key to success "
+    },
+    { username : "Manisha",
+      content  : "I Love Coding ! "
+    },
+    { username : "Anju",
+      content  : "Keep Working Hard ! "
+    },
+    { username : "Sourav",
+      content  : "Be consistent ."
+    },
+    
+]
+
+
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
+});
+
+app.listen(port,()=>{
+    console.log("Listening to port :8080");
+})
