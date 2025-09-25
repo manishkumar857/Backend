8eb1b0baddbea530e523ea2c063cb9c4b73e7bdc
Manish Kumar <jangramanish2602@gmail.com>
2025-09-25 18:43:04 +0530

    new post

--- REST_CLASS/index.js
+++ REST_CLASS/index.js
@@ -31,6 +31,17 @@ app.get("/posts",(req,res)=>{
     res.render("index.ejs",{posts});
 });
 
+app.get("/posts/new",(req,res)=>{
+  res.render("new.ejs")
+});
+
+app.post("/posts",(req,res)=>{
+  
+  let {username,content}=req.body;
+  posts.push({username,content});
+  res.send("post request working");
+})
+
 app.listen(port,()=>{
     console.log("Listening to port :8080");
 })
